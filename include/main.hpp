aa2aa94bc9d1a6d654824eb86144b1dd237bac99
SAITO, Tetsuya <saito.tetsuya@gmail.com>
2024-04-19 01:14:08 +0900

    M5DEV_LOGの追加

--- include/main.hpp
+++ include/main.hpp
@@ -156,3 +156,17 @@ inline void M5_UPDATE(void) {
     M5DEV.update();
 #endif
 }
+
+#if defined(USE_M5UNIFIED)
+#define M5DEV_LOGE M5_LOGE
+#define M5DEV_LOGW M5_LOGW
+#define M5DEV_LOGI M5_LOGI
+#define M5DEV_LOGD M5_LOGD
+#define M5DEV_LOGV M5_LOGV
+#else
+#define M5DEV_LOGE(format, ...) log_e("[%s] " format, "M5BP", ##__VA_ARGS__)
+#define M5DEV_LOGW(format, ...) log_w("[%s] " format, "M5BP", ##__VA_ARGS__)
+#define M5DEV_LOGI(format, ...) log_i("[%s] " format, "M5BP", ##__VA_ARGS__)
+#define M5DEV_LOGD(format, ...) log_d("[%s] " format, "M5BP", ##__VA_ARGS__)
+#define M5DEV_LOGV(format, ...) log_v("[%s] " format, "M5BP", ##__VA_ARGS__)
+#endif
